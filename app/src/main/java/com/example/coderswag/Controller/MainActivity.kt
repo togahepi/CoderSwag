4968c40eff2d8a0552ece816df406a2cb786f965
Togahepi <hiepgato@gmail.com>
2019-05-04 08:23:14 +0700

    arrayAdapter only work with simple_list_view_1 not 2

--- app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
@@ -4,20 +4,21 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import com.example.coderswag.Model.Category
+import com.example.coderswag.Model.Product
 import com.example.coderswag.R
 import com.example.coderswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : ArrayAdapter<Product>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         adapter = ArrayAdapter(this,
-            android.R.layout.simple_list_item_2, DataService.categories)
+            android.R.layout.simple_list_item_1, DataService.hats)
 
         categoryListView.adapter = adapter
     }
--- app/src/main/java/com/example/coderswag/Services/DataService.kt
+++ app/src/main/java/com/example/coderswag/Services/DataService.kt
@@ -8,7 +8,7 @@ object DataService {
         Category("SHIRTS", "shirtimage"),
         Category("HOODIES", "hoodieimage"),
         Category("HATS", "hatimage"),
-        Category("DIGITALGOODSIMAGE","digitalgoodsimage")
+        Category("DIGITAL","digitalgoodsimage")
     )
     val hats = listOf(
         Product("Devslopes Graphic Beanie","$18","hat01"),
