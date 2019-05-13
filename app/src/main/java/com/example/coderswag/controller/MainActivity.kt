e507aa018a923ebd6b5d3918ebe56c40446066bf
Togahepi <hiepgato@gmail.com>
2019-05-13 08:34:15 +0700

    Set on item clicked

--- app/src/main/java/com/example/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/controller/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.coderswag.controller
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import com.example.coderswag.R
 import com.example.coderswag.adapters.CategoryAdapter
+import com.example.coderswag.model.Category
 import com.example.coderswag.services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,7 +18,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         adapter = CategoryAdapter(this, DataService.categories)
-
         categoryListView.adapter = adapter
+        
+        categoryListView.setOnItemClickListener { _, _, position, _ ->
+            val category = DataService.categories[position]
+            Toast.makeText(this, "You have clicked ${category.title} cell!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
