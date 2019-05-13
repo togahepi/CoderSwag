bd842ca8f408e5e6c56a7addf6069bc78da50885
Togahepi <hiepgato@gmail.com>
2019-05-13 16:04:30 +0700

    Product List layout and creat a function on DataService using when switch case to return the list from the title of category

--- app/src/main/java/com/example/coderswag/services/DataService.kt
+++ app/src/main/java/com/example/coderswag/services/DataService.kt
@@ -23,22 +23,32 @@ object DataService {
         Category("DIGITAL","digitalgoodsimage")
     )
     val hats = listOf(
-        Product("Devslopes Graphic Beanie","$18","hat01"),
-        Product("Devslopes Hat Black","$20","hat02"),
-        Product("Devslopes Hat While","$19","hat03"),
-        Product("Devslopes Hat Snapback","$22","hat04")
+        Product("Devslopes Graphic Beanie","$18","hat1"),
+        Product("Devslopes Hat Black","$20","hat2"),
+        Product("Devslopes Hat While","$19","hat3"),
+        Product("Devslopes Hat Snapback","$22","hat4")
     )
     val hoodies = listOf(
-        Product("Devslopes Hoodie Gray","$20","hoodie01"),
-        Product("Devslopes Hoodie Red","$24","hoodie02"),
-        Product("Devslopes Hoodie Black","$23","hoodie03"),
-        Product("Devslopes Hoodie Kind Of Black","$200","hoodie04")
+        Product("Devslopes Hoodie Gray","$20","hoodie1"),
+        Product("Devslopes Hoodie Red","$24","hoodie2"),
+        Product("Devslopes Hoodie Black","$23","hoodie3"),
+        Product("Devslopes Hoodie Kind Of Black","$200","hoodie4")
     )
     val shirts = listOf(
-        Product("Devslopes Shirt Black", "$34","shirt01"),
-        Product("Devslopes Badge Light Gray", "$99","shirt02"),
-        Product("Devslopes Logo Shirt Red", "$990","shirt03"),
-        Product("Devslopes Hustle", "$19","shirt04"),
-        Product("Kicklips Studio", "$9999","shirt05")
+        Product("Devslopes Shirt Black", "$34","shirt1"),
+        Product("Devslopes Badge Light Gray", "$99","shirt2"),
+        Product("Devslopes Logo Shirt Red", "$990","shirt3"),
+        Product("Devslopes Hustle", "$19","shirt4"),
+        Product("Kicklips Studio", "$9999","shirt5")
     )
+    val digitalGood = listOf<Product>()
+
+    fun getCategory(category: String) : List<Product> {
+        return when (category) {
+            "SHIRTS" -> shirts
+            "HATS" -> hats
+            "HOODIES" -> hoodies
+            else -> digitalGood
+        }
+    }
 }
