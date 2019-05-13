f2b9f90ddd210f31195c473914507d3460f08bad
Togahepi <hiepgato@gmail.com>
2019-05-13 10:12:45 +0700

    refactor some typing

--- app/src/main/java/com/example/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/controller/MainActivity.kt
@@ -3,17 +3,14 @@ package com.example.coderswag.controller
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.widget.Toast
 import com.example.coderswag.R
-import com.example.coderswag.adapters.CategoryAdapter
 import com.example.coderswag.adapters.CategoryRecycleAdapter
-import com.example.coderswag.model.Category
 import com.example.coderswag.services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : CategoryRecycleAdapter
+    lateinit var adapter: CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
